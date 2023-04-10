0fe0a64c660255741a8799656d9ad5cd7e7b9fe7
abhimanyubangaru <48572985+abhimanyubangaru@users.noreply.github.com>
2023-04-10 19:57:00 -0400

    1 file modified

--- src/Page.js
+++ src/Page.js
@@ -29,7 +29,7 @@ export default class Page extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      task:  {},
+      task: {},
       tasks: [],
       open: false,
       add: false,
@@ -70,16 +70,13 @@ export default class Page extends Component {
 
   //Callback from dialog input to see what next to do
   dialogCallback = (data) => {
-    
     if (data.action === 'submit') {
-      console.log("reached here");
+      console.log('reached here');
       this.addSuccess();
       this.setState({ tasks: [...this.state.tasks, data.task] });
       this.handleClose();
-      console.log(
-        "printing tasks"
-      );
-      console.log(this.state.tasks)
+      console.log('printing tasks');
+      console.log(this.state.tasks);
     } else if (data.action === 'cancel') {
       this.handleClose();
     } else if (data.action === 'edit') {
@@ -98,116 +95,139 @@ export default class Page extends Component {
   render() {
     return (
       <>
-         <DiaWrap open={this.state.open} onClose={() => this.dialogCallback()}>
-            <DialogTask
-              add={this.state.add}
-              index={this.state.index}
-              data={this.state.data}
-              dataFromParents={this.state.task}
-              parentCallback={this.dialogCallback}
-              tasks={this.state.tasks}
-            ></DialogTask>
-          </DiaWrap>
+        <DiaWrap
+          open={this.state.open}
+          onClose={() => this.dialogCallback()}
+          disableBackdropClick={true}
+        >
+          <DialogTask
+            add={this.state.add}
+            index={this.state.index}
+            data={this.state.data}
+            dataFromParents={this.state.task}
+            parentCallback={this.dialogCallback}
+            tasks={this.state.tasks}
+          ></DialogTask>
+        </DiaWrap>
         {/*HEADER*/}
-        <Card  >
-        <CardHeader
-          sx={{ bgcolor: 'primary.dark', color: 'white' }}
-          title={
-            <>
-              <span>
-                <MenuIcon className="svg_icons" id="header" />
-                FRAMEWORKS
-              </span>
-            </>
-          }
-          style={{ textAlign: 'center' }}
-          action={
-            <>
-              <Button
-                variant="contained"
-                onClick={() => this.addATask()}
-                sx={{ marginRight: '5%' }}
-              >
-                <AddCircleIcon />
-                &nbsp;ADD
-              </Button>
-            </>
-          }
-        />
-        <CardContent sx = {{bgcolor: 'white', marginBottom: -1 }}>
-          <TableContainer>
-            <Table sx ={{bgcolor: 'white'}}>
-              <TableHead>
-                <TableRow>
-                  <TableCell align="center" sx={{ color: 'grey' }}>
-                    Title
-                  </TableCell>
-                  <TableCell align="center" sx={{ color: 'grey' }}>
-                    Description
-                  </TableCell>
-                  <TableCell align="center" sx={{ color: 'grey' }}>
-                    Deadline
-                  </TableCell>
-                  <TableCell align="center" sx={{ color: 'grey' }}>
-                    Priority
-                  </TableCell>
-                  <TableCell align="center" sx={{ color: 'grey' }}>
-                    Is Complete
-                  </TableCell>
-                  <TableCell align="center" sx={{ color: 'grey' }}>
-                    Action
-                  </TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {this.state.tasks.map((thisTask, index) => {
-                  console.log(thisTask); 
-                  return (
-                    <TableRow key={thisTask.title}>
-                      <TableCell align="center">{thisTask.title}</TableCell>
-                      <TableCell align="center">
-                        {thisTask.description}
-                      </TableCell>
-                      <TableCell align="center">
-                        {moment(thisTask.deadline).format("MM/DD/YY")}
-                      </TableCell>
-                      <TableCell align="center">{thisTask.priority}</TableCell>
-                      <TableCell align="center">{thisTask.checked ? <Checkbox checked={thisTask.checked} onChange={(e) => {this.toggleIsComplete(index)}}/> :
-                        <Checkbox
-                          
-                          checked={thisTask.checked}
-                          onChange={(e) => {this.toggleIsComplete(index)}}
-                        /> }
-                      </TableCell>
-                      <TableCell align="center">
-                       {!thisTask.checked && <Button variant="contained" sx={{width : 100}} sx={{ width: '85%' }}
-                       onClick={() => this.editThisEntry(index)}>
-                       <EditIcon fontSize="small" />
-                                &nbsp;Update
-                              </Button>
-                         }
-                         <Button
-                          color="error"
-                          variant="contained"
-                          onClick={() => {this.setState((prevState) => ({
-                            tasks: [...prevState.tasks.slice(0,index),...prevState.tasks.slice(index+1)]
-                          }));
-                            this.deleteSuccess();
-                          }}
-                          sx={{ bgcolor: 'red', width: '85%' }}
-                        >
-                          <CancelIcon fontSize="small" />
-                          &nbsp;Delete
-                        </Button>
-                      </TableCell>
-
-                    </TableRow>
-                  );
-                })}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </CardContent>
+        <Card>
+          <CardHeader
+            sx={{ bgcolor: 'primary.dark', color: 'white' }}
+            title={
+              <>
+                <span>
+                  <MenuIcon className="svg_icons" id="header" />
+                  FRAMEWORKS
+                </span>
+              </>
+            }
+            style={{ textAlign: 'center' }}
+            action={
+              <>
+                <Button
+                  variant="contained"
+                  onClick={() => this.addATask()}
+                  sx={{ marginRight: '5%' }}
+                >
+                  <AddCircleIcon />
+                  &nbsp;ADD
+                </Button>
+              </>
+            }
+          />
+          <CardContent sx={{ bgcolor: 'white', marginBottom: -1 }}>
+            <TableContainer>
+              <Table sx={{ bgcolor: 'white' }}>
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="center" sx={{ color: 'grey' }}>
+                      Title
+                    </TableCell>
+                    <TableCell align="center" sx={{ color: 'grey' }}>
+                      Description
+                    </TableCell>
+                    <TableCell align="center" sx={{ color: 'grey' }}>
+                      Deadline
+                    </TableCell>
+                    <TableCell align="center" sx={{ color: 'grey' }}>
+                      Priority
+                    </TableCell>
+                    <TableCell align="center" sx={{ color: 'grey' }}>
+                      Is Complete
+                    </TableCell>
+                    <TableCell align="center" sx={{ color: 'grey' }}>
+                      Action
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {this.state.tasks.map((thisTask, index) => {
+                    console.log(thisTask);
+                    return (
+                      <TableRow key={thisTask.title}>
+                        <TableCell align="center">{thisTask.title}</TableCell>
+                        <TableCell align="center">
+                          {thisTask.description}
+                        </TableCell>
+                        <TableCell align="center">
+                          {moment(thisTask.deadline).format('MM/DD/YY')}
+                        </TableCell>
+                        <TableCell align="center">
+                          {thisTask.priority}
+                        </TableCell>
+                        <TableCell align="center">
+                          {thisTask.checked ? (
+                            <Checkbox
+                              checked={thisTask.checked}
+                              onChange={(e) => {
+                                this.toggleIsComplete(index);
+                              }}
+                            />
+                          ) : (
+                            <Checkbox
+                              checked={thisTask.checked}
+                              onChange={(e) => {
+                                this.toggleIsComplete(index);
+                              }}
+                            />
+                          )}
+                        </TableCell>
+                        <TableCell align="center">
+                          {!thisTask.checked && (
+                            <Button
+                              variant="contained"
+                              sx={{ width: '85%' }}
+                              onClick={() => this.editThisEntry(index)}
+                            >
+                              <EditIcon fontSize="small" />
+                              &nbsp;Update
+                            </Button>
+                          )}
+                          <Button
+                            color="error"
+                            variant="contained"
+                            onClick={() => {
+                              this.setState((prevState) => ({
+                                tasks: [
+                                  ...prevState.tasks.slice(0, index),
+                                  ...prevState.tasks.slice(index + 1),
+                                ],
+                              }));
+                              this.deleteSuccess();
+                            }}
+                            sx={{ bgcolor: 'red', width: '85%' }}
+                          >
+                            <CancelIcon fontSize="small" />
+                            &nbsp;Delete
+                          </Button>
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </CardContent>
         </Card>
         {/*TOASTER CONTAINER SO TOASTS CAN DISPLAY */}
         <ToastContainer
